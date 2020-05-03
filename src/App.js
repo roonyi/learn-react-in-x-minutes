cfff7cd3debfd63711b28f56f74656dd9a8c8dbc
Roberto <Roonyi@gmail.com>
2020-05-03 18:17:01 -0500

    Learn React Today Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Today with
+          Learn React Today with master
         </a>
       </header>
     </div>
